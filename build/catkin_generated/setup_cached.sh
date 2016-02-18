82e15f2dbac5c85d1e8d3ef733708b5a861396ea
clm <clm@clm-PC.(none)>
2016-02-18 17:54:55 +0000

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/clm/Aran/wip_ws/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/clm/Aran/wip_ws/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/clm/Aran/wip_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/clm/Aran/wip_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/clm/Aran/wip_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/clm/Aran/wip_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/clm/Aran/wip_ws/devel/lib/pkgconfig:/opt/ros/hydro/lib/pkgconfig:/usr/local/lib/pkgconfig"
+export PWD="/home/clm/Aran/wip_ws/build"
+export PYTHONPATH="/home/clm/Aran/wip_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/clm/Aran/wip_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/clm/Aran/wip_ws/src:$ROS_PACKAGE_PATH"
