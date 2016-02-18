df07e12fdb00f09841f4ba4c66d7100b26f25bc4
clm <clm@clm-PC.(none)>
2016-02-18 21:21:48 +0000

    start on tf stuff

---
+++ build/wip_tf/CMakeFiles/tf_listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tf_listener.dir/src/tf_listener.cpp.o"
+  "/home/clm/Aran/wip_ws/devel/lib/wip_tf/tf_listener.pdb"
+  "/home/clm/Aran/wip_ws/devel/lib/wip_tf/tf_listener"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tf_listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
