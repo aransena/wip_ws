f0bdeca766828609fe2d2ce0877fdee114080b8f
aransena <aransena@gmail.com>
2016-05-16 18:17:13 +0100

    visualisation

--- src/sunset_demo/src/rosbag_analysis.py
+++ src/sunset_demo/src/rosbag_analysis.py
@@ -11,6 +11,24 @@ dir = os.path.expanduser("~")
 print "DIR: ", dir
 #map_file = os.path.join(dir, 'wip_ws/src/robot_2dnav/maps/map.pgm')
 map_file = os.path.join(dir,'/opt/ros/indigo/share/turtlebot_gazebo/maps/playground.pgm')
+map_yaml = os.path.join(dir,'/opt/ros/indigo/share/turtlebot_gazebo/maps/playground.yaml')
+
+origin_offset_x = 0
+origin_offset_y = 0
+map_scale = 0
+
+f = open(map_yaml,'r')
+for line in f:
+    if "origin" in line:
+        origin_offset_x = float(line.split()[1][1:-1])
+        origin_offset_y = float(line.split()[2][:-1])
+        print origin_offset_x, " ", origin_offset_y
+
+    if "resolution" in line:
+        map_scale = float(line.split()[1])
+
+f.close()
+
 bag = rosbag.Bag('test.bag')
 print bag
 X=[]
@@ -23,8 +41,8 @@ y = 0
 x_prev = 0
 y_prev = 0
 dist_tot = 0
-origin_offset_x = -6.89999
-origin_offset_y = -5.89999
+#origin_offset_x = -6.89999
+#origin_offset_y = -5.89999
 
 for topic, msg, t in rosbag.Bag('test.bag').read_messages():
 #for topic, msg, t in bag.read_messages(topics=['amcl_pose']):
@@ -34,8 +52,8 @@ for topic, msg, t in rosbag.Bag('test.bag').read_messages():
     if topic == "/amcl_pose":#print topic
         point = PoseWithCovarianceStamped()
         point = msg
-        x = (point.pose.pose.position.x - origin_offset_x)*20
-        y = (point.pose.pose.position.y - origin_offset_y)*20
+        x = (point.pose.pose.position.x - origin_offset_x)*(1/map_scale)
+        y = (point.pose.pose.position.y - origin_offset_y)*(1/map_scale)
         if(firstRun==True):
             x_prev = x
             y_prev = y
@@ -59,6 +77,6 @@ plt.figure(1)
 img =np.flipud(img)
 y1=img.shape[0]
 x1=img.shape[1]
-plt.imshow(img,zorder=0,origin='lower', extent=[0,x1,0,y1])
+plt.imshow(img,zorder=0,origin='lower', extent=[0,x1,0,y1],cmap=plt.cm.gray)
 plt.plot(X,Y)
 plt.show()
